b0570370e4d4719cc8a7581c86de1da143f21111
ariwansohn <ariwansohn@gmail.com>
2020-04-21 11:20:21 +0900

    v0

---
+++ precache-manifest.ece9336f7ed9aa9c01236de7659f52ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7877c4475c7b3c2a1954d4fbbf4f3ac9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a85f91df496b5ab20d61",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "83c6d1c6a71184a71800",
+    "url": "/static/js/2.ac554477.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ac554477.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a85f91df496b5ab20d61",
+    "url": "/static/js/main.3aeff5dd.chunk.js"
+  },
+  {
+    "revision": "af7b873c7a75ce3526f1",
+    "url": "/static/js/runtime-main.36e1cc88.js"
+  }
+]);
